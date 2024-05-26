d9f57303e5c32684a9e4be577dea88a3853a5a1e
oleojake <oleojake@gmail.com>
2024-05-26 10:31:19 +0200

    footer

--- src/components/WeatherApiSignature.tsx
+++ src/components/WeatherApiSignature.tsx
@@ -5,13 +5,13 @@ export const WeatherApiSignature: React.FC = () => {
 		<Box sx={{ display: "flex", justifyContent: "space-evenly", my: 1 }}>
 			<Typography textAlign="center" sx={{ fontSize: "10px" }}>
 				Github:{" "}
-				<a href="https://www.weatherapi.com/" title="Weather API">
+				<a href="https://github.com/oleojake" title="Github" target="_blank">
 					OleojakeDev
 				</a>
 			</Typography>
 			<Typography textAlign="center" sx={{ fontSize: "10px" }}>
 				Powered by:{" "}
-				<a href="https://www.weatherapi.com/" title="Weather API">
+				<a href="https://www.weatherapi.com/" title="Weather API" target="_blank">
 					WeatherAPI.com
 				</a>
 			</Typography>
