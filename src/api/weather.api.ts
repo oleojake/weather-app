89a65974d79a73d8cbbfa91f8de313ce730f7b52
oleojake <oleojake@gmail.com>
2024-05-26 10:14:16 +0200

    api https request

--- src/api/weather.api.ts
+++ src/api/weather.api.ts
@@ -1,6 +1,6 @@
 import { CurrentWeather } from "./model.api";
 
-const API_WEATHER = `http://api.weatherapi.com/v1/forecast.json?key=${import.meta.env.VITE_API_KEY}&lang=en&days=14&q=`;
+const API_WEATHER = `https://api.weatherapi.com/v1/forecast.json?key=${import.meta.env.VITE_API_KEY}&lang=en&days=14&q=`;
 
 export const getCurrentWeather = async (city: string): Promise<CurrentWeather> => {
 	const res = await fetch(API_WEATHER + city);
